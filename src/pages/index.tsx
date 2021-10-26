407202407c38177446c3d842c078d5f0ad97924e
Diego Albitres <diego.albitres@gmail.com>
2021-10-25 23:43:12 -0500

    Minor responsive tweaks and added App Store badge

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import YOUniversity from '../../public/youniversity.svg';
 import GooglePlayBadge from '../../public/google-play-badge.svg';
+import AppStoreBadge from '../../public/app-store-badge.svg';
 import DividerSvg from '../../public/divider.svg';
 
 const Home = (): React.ReactElement | null => {
@@ -13,7 +14,7 @@ const Home = (): React.ReactElement | null => {
         <div className="hidden lg:block absolute min-h-screen max-h-screen top-0 right-0 z-0">
           <DividerSvg className="min-h-screen max-h-screen w-auto text-primary" />
         </div>
-        <div className="px-8 lg:px-0 lg:pt-24 z-10">
+        <div className="px-8 lg:px-0 z-10 flex flex-col justify-center">
           <div className="flex flex-col lg:flex-row items-center">
             <YOUniversity className="h-16 lg:h-32 w-auto" />
             <h1 className="mt-4 lg:mt-0 lg:ml-4 text-4xl lg:text-7xl text-primary font-semibold">
@@ -31,8 +32,9 @@ const Home = (): React.ReactElement | null => {
           <div className="mt-4 lg:mt-8 text-xl text-gray-600 font-medium text-center lg:text-left">
             La organización que siempre quisiste, ahora en tu teléfono.
           </div>
-          <div className="mt-8 flex justify-center lg:justify-start">
-            <GooglePlayBadge className="h-14 sm:h-16 w-auto" />
+          <div className="mt-8 flex flex-col lg:flex-row justify-center lg:justify-start items-center">
+            <GooglePlayBadge className="h-14 w-auto" />
+            <AppStoreBadge className="h-14 w-auto mt-4 lg:mt-0 lg:ml-4" />
           </div>
         </div>
         <div className="mt-8 lg:mt-0 py-10 px-8 lg:py-0 lg:px-0 bg-primary lg:bg-transparent w-auto flex items-center justify-center lg:justify-end z-10">
