4de13fd60218ff0a9b0b8667e6e28ffd76b2ca4d
Diego Albitres <diego.albitres@gmail.com>
2021-11-15 17:45:26 -0500

    feat: add redirect to Figma prototype

--- next.config.js
+++ next.config.js
@@ -2,11 +2,21 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === "true",
 });
 
-module.exports = withBundleAnalyzer({
+/** @type {import('next').NextConfig} */
+const config = {
   images: {
     domains: JSON.parse(process.env.STATIC_CONTENT_DOMAIN || "[]"),
   },
   productionBrowserSourceMaps: process.env.PRODUCTION_SOURCE_MAPS === "true",
+  redirects: async () => {
+    return [
+      {
+        source: '/prototipo',
+        destination: 'https://www.figma.com/proto/neNIFk5WvrKhnBHxqsryWo/Android-Application-Mock-ups',
+        permanent: false,
+      },
+    ];
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
@@ -15,4 +25,6 @@ module.exports = withBundleAnalyzer({
 
     return config;
   },
-});
+};
+
+module.exports = withBundleAnalyzer(config);
