33ee1c329fb20b49d35f09eec4a2de9afbc8ef56
Diego Albitres <diego.albitres@gmail.com>
2022-09-19 03:14:54 -0500

    chore: remove Next.js redirect

--- next.config.js
+++ next.config.js
@@ -8,15 +8,6 @@ const config = {
     domains: JSON.parse(process.env.STATIC_CONTENT_DOMAIN || "[]"),
   },
   productionBrowserSourceMaps: process.env.PRODUCTION_SOURCE_MAPS === "true",
-  redirects: async () => {
-    return [
-      {
-        source: '/prototipo',
-        destination: 'https://www.figma.com/proto/neNIFk5WvrKhnBHxqsryWo/Android-Application-Mock-ups',
-        permanent: false,
-      },
-    ];
-  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
