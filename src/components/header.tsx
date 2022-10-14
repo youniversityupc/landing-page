7526fb9a3ac09221575c80a45b15442ac49dc0bf
Diego Albitres <diego.albitres@gmail.com>
2022-10-14 02:30:37 -0500

    feat: added initial header implementation

---
+++ src/components/header.tsx
@@ -0,0 +1,109 @@
+import Link from 'next/link';
+import { Fragment } from 'react';
+import { Popover, Transition } from '@headlessui/react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import YOUniversity from '../../public/youniversity.svg';
+import clsx from 'clsx';
+
+interface NavItem {
+  className?: string,
+  labelClassName?: string,
+  label: string,
+  href: string,
+  shallow?: boolean,
+}
+
+const navigation: NavItem[] = [
+  {
+    label: "Características",
+    href: "/#features",
+    shallow: true,
+  },
+];
+
+interface HeaderProps {
+  className?: string,
+}
+
+export const Header = (props: HeaderProps): React.ReactElement => {
+  return (
+    <header className={clsx(props.className, "h-24 sticky top-0 w-full z-50 transition-top")}>
+      <Popover className="relative w-full">
+        <div className="mx-auto max-w-7xl px-6">
+          <div className="flex items-center justify-between py-6 md:space-x-10">
+            <div className="flex justify-start">
+              <Link href="/#" shallow scroll>
+                <a className="flex items-center space-x-4">
+                  <YOUniversity className="h-10 w-auto" />
+                  <span className="text-2xl text-primary font-semibold hidden sm:inline">
+                    YOUniversity
+                  </span>
+                </a>
+              </Link>
+            </div>
+            <div className="-my-2 -mr-2 md:hidden">
+              <Popover.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                <span className="sr-only">Abrir menú</span>
+                <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+              </Popover.Button>
+            </div>
+            <nav className="hidden space-x-10 md:flex">
+              {navigation.map(item => (
+                <Link key={item.href} href={item.href} shallow={item.shallow} scroll>
+                  <a className={clsx(item.className, "text-base font-medium text-gray-500 hover:text-gray-900")}>
+                    <span className={item.labelClassName}>
+                      {item.label}
+                    </span>
+                  </a>
+                </Link>
+              ))}
+            </nav>
+          </div>
+        </div>
+
+        <Transition
+          as={Fragment}
+          enter="duration-200 ease-out"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="duration-100 ease-in"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          <Popover.Panel focus className="absolute inset-x-0 top-0 origin-top-right transform p-2 transition md:hidden">
+            {({ close }) => (
+              <div className="rounded-lg bg-background shadow-lg ring-1 ring-black ring-opacity-5">
+                <div className="px-5 pt-5 pb-6">
+                  <div className="flex items-center justify-between">
+                    <div>
+                      <YOUniversity className="h-10 w-auto" />
+                    </div>
+                    <div className="-mr-2">
+                      <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                        <span className="sr-only">Cerrar menú</span>
+                        <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                      </Popover.Button>
+                    </div>
+                  </div>
+                  <div className="mt-6">
+                    <nav className="grid gap-y-8">
+                      {navigation.map(item => (
+                        <Link key={item.href} href={item.href} shallow={item.shallow} scroll>
+                          <a className={clsx(item.className, "-m-3 flex items-center rounded-md p-3 hover:bg-gray-50")} onClick={() => close()}>
+                            <span className={clsx(item.labelClassName, "ml-3 text-base font-medium text-gray-900")}>
+                              {item.label}
+                            </span>
+                          </a>
+                        </Link>
+                      ))}
+                    </nav>
+                  </div>
+                </div>
+              </div>
+            )}
+          </Popover.Panel>
+        </Transition>
+      </Popover>
+    </header>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,57 +2,63 @@ import Head from 'next/head';
 import YOUniversity from '../../public/youniversity.svg';
 import GooglePlayBadge from '../../public/google-play-badge.svg';
 import Feature from '../components/feature';
+import { Header } from "../components/header";
 
-const Home = (): React.ReactElement | null => {
+const Home = (): React.ReactElement => {
   return (
-    <main>
+    <>
       <Head>
         <title>YOUniversity</title>
       </Head>
-      <div className="bg-background lg:min-h-screen lg:max-h-screen flex flex-col lg:flex-row py-8 lg:px-16 lg:py-0">
-        <div className="px-8 lg:px-0 z-10 flex flex-col justify-center w-full">
-          <div className="flex flex-col lg:flex-row items-center justify-center">
-            <YOUniversity className="h-16 lg:h-32 w-auto" />
-            <h1 className="mt-4 lg:mt-0 lg:ml-4 text-4xl lg:text-7xl text-primary font-semibold">
-              YOUniversity
-            </h1>
+      <Header className="bg-background flex items-center" />
+      <main>
+        <section className="bg-background flex flex-col lg:flex-row py-24 lg:px-16">
+          <div className="px-8 lg:px-0 z-10 flex flex-col justify-center w-full">
+            <div className="flex flex-col lg:flex-row items-center justify-center">
+              <YOUniversity className="h-16 lg:h-32 w-auto" />
+              <h1 className="mt-4 lg:mt-0 lg:ml-4 text-4xl lg:text-7xl text-primary font-semibold">
+                YOUniversity
+              </h1>
+            </div>
+            <div className="mt-8 lg:mt-16 text-3xl lg:text-5xl text-primary-darker font-medium text-center">
+              <span className="block">
+                La <span className="text-primary font-bold">universidad</span> a
+              </span>
+              <span className="block">
+                <span className="text-primary font-bold">tu</span> manera
+              </span>
+            </div>
+            <div className="mt-4 lg:mt-8 text-xl text-gray-600 font-medium text-center">
+              La organización que siempre quisiste, ahora en tu teléfono.
+            </div>
+            <div className="mt-8 flex justify-center items-center">
+              <GooglePlayBadge className="h-14 w-auto" />
+            </div>
           </div>
-          <div className="mt-8 lg:mt-16 text-3xl lg:text-5xl text-primary-darker font-medium text-center">
-            <span className="block">
-              La <span className="text-primary font-bold">universidad</span> a
-            </span>
-            <span className="block">
-              <span className="text-primary font-bold">tu</span> manera
-            </span>
-          </div>
-          <div className="mt-4 lg:mt-8 text-xl text-gray-600 font-medium text-center">
-            La organización que siempre quisiste, ahora en tu teléfono.
-          </div>
-          <div className="mt-8 flex justify-center items-center">
-            <GooglePlayBadge className="h-14 w-auto" />
-          </div>
-        </div>
-      </div>
-      <Feature
-        text="Obtén un resumen diario de actividades pendientes"
-        imageUrl="/home@2x.png"
-        divClassName="bg-primary"
-        textClassName="text-background text-4xl"
-      />
-      <Feature
-        inverted
-        text="Organiza y añade tus horarios para mayor variedad"
-        imageUrl="/timetable@2x.png"
-        divClassName="bg-background"
-        textClassName="text-primary text-4xl"
-      />
-      <Feature
-        text="Registra tus cursos y la información correspondiente a cada uno"
-        imageUrl="/courses@2x.png"
-        divClassName="bg-primary"
-        textClassName="text-background text-4xl"
-      />
-    </main>
+        </section>
+        <section id="features" className="flex flex-col">
+          <Feature
+            text="Obtén un resumen diario de actividades pendientes"
+            imageUrl="/home@2x.png"
+            divClassName="bg-primary"
+            textClassName="text-background text-4xl"
+          />
+          <Feature
+            inverted
+            text="Organiza y añade tus horarios para mayor variedad"
+            imageUrl="/timetable@2x.png"
+            divClassName="bg-background"
+            textClassName="text-primary text-4xl"
+          />
+          <Feature
+            text="Registra tus cursos y la información correspondiente a cada uno"
+            imageUrl="/courses@2x.png"
+            divClassName="bg-primary"
+            textClassName="text-background text-4xl"
+          />
+        </section>
+      </main>
+    </>
   );
 };
 
