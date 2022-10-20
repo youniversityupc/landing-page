146fd26870b6e930091092ad85604f817c1f85ef
Diego Albitres <diego.albitres@gmail.com>
2022-10-20 12:14:58 -0500

    fix: incorrect bg of close button

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -92,7 +92,7 @@ export const Header = (props: HeaderProps): React.ReactElement => {
                       <YOUniversity className="h-10 w-auto" />
                     </div>
                     <div className="-mr-2">
-                      <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                      <Popover.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                         <span className="sr-only">Cerrar menú</span>
                         <XMarkIcon className="h-6 w-6" aria-hidden="true" />
                       </Popover.Button>
